170e2c59b2304138d4421230cfdb24335f6c284c
YENUGULA SURYA NAGA SIVARAM <sivaramyenugula.2003@gmail.com>
2025-09-10 16:13:17 +0530

    Add files via upload

---
+++ static/main.js
@@ -0,0 +1,4 @@
+// Minimal JS hook for future add-ons.
+// For this starter, we don't need JS logic.
+// You can add client-side validations or UI effects here.
+console.log("Mini Event System loaded");
